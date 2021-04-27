2c1df8a1a97816355949a9538acf99526be59b99
gopher <gopher.hu@gmail.com>
2021-04-27 10:18:33 +0200

    Path fixes.

--- AdminApp.php
+++ AdminApp.php
@@ -4,15 +4,20 @@ class AdminApp extends App {
 
     public function __construct($env='dev', $configPath='config.ini.php') {
         parent::__construct($env, $configPath);
-        $this->addModule('UsersModule');
+        $this->addModule('AdminModule');
         $this->addModule('BulmaModule');
+        $this->addModule('UsersModule');
+        $this->addModule('UsersAdminModule');
     }
 
     public function init() {
         parent::init();
         $framework = Framework::instance();
-        $this->translation->add('admin', 'modules/minicore-admin/translations');
-        $this->view->addFolder(':admin', 'modules/minicore-admin/templates');
+        $app = $framework->get('app');
+        $module = $app->getModule('minicore-admin');
+        $folder = $module->getFolder();
+        $this->translation->add('admin', $folder.'translations');
+        $this->view->addFolder(':admin', $folder.'templates');
         $this->view->changePath(':app/layout', ':admin/layout');
         $this->view->changePath(':pager/pager', ':admin/pager');
         $this->view->changePath(':user/login', ':admin/user/login');
